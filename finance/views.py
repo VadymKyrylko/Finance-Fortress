48a3e13dd960515b9611a28026f8537741293723
Vadym <kirilkovadim193@gmail.com>
2025-11-20 18:16:32 +0200

    FEAT: Implement AccountListView (CBV) and BootStrap base template.

--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views.generic import ListView
+from finance.models import Account
 
-# Create your views here.
+
+class AccountListView(LoginRequiredMixin, ListView):
+    model = Account
+    template_name = "finance/account_list.html"
+    context_object_name = "accounts"
+
+    def get_queryset(self):
+        return Account.objects.filter(user=self.request.user).order_by("name")
