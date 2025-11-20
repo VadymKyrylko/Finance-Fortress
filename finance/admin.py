f86af00dbfe9613c869b42f067f50375dcb28441
Vadym <kirilkovadim193@gmail.com>
2025-11-20 17:07:05 +0200

    Add core finance models and admin setup

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
+from finance.models import (
+    Account,
+    Category,
+    Transaction,
+    Budget,
+    ExchangeRate,
+    Tag,
+)
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("name", "balance", "currency", "user")
+    list_filter = ("user", "currency")
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "type", "user")
+    list_filter = ("type", "user")
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ("user", "type", "amount", "account", "category", "date")
+    list_filter = ("type", "date", "user")
+    date_hierarchy = "date"
+
+
+@admin.register(Budget)
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ("user", "category", "amount_limit", "month")
+
+
+admin.site.register(ExchangeRate)
+admin.site.register(Tag)
