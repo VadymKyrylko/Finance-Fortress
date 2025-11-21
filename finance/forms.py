22f42c59b3f4f103dbd2fc48fbfa8fb89ceb1d7b
Vadym <kirilkovadim193@gmail.com>
2025-11-21 12:55:46 +0200

    FEAT: Implement Transaction creation flow(Form, CreateView, Template)

---
+++ finance/forms.py
@@ -0,0 +1,49 @@
+from django import forms
+from finance.models import Transaction, Category, Account
+
+
+class TransactionForm(forms.ModelForm):
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.none(),
+        empty_label="Choose Category",
+        required=False,
+        label="Category",
+    )
+
+    class Meta:
+        model = Transaction
+        fields = (
+            "type",
+            "amount",
+            "account",
+            "category",
+            "target_account",
+            "description",
+        )
+        widgets = {
+            "amount": forms.NumberInput(
+                attrs={"class": "form-control", "placeholder": "0.00"}
+            ),
+            "account": forms.Select(attrs={"class": "form-select"}),
+            "type": forms.Select(attrs={"class": "form-select"}),
+            "category": forms.Select(attrs={"class": "form-select"}),
+            "target_account": forms.Select(attrs={"class": "form-select"}),
+            "description": forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                    "rows": "3",
+                    "placeholder": "Description",
+                }
+            ),
+        }
+
+    def __init__(self, *args, user=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if user:
+            self.fields["category"].queryset = Category.objects.filter(
+                user=user
+            )
+            self.fields["account"].queryset = Account.objects.filter(user=user)
+            self.fields["target_account"].queryset = Account.objects.filter(
+                user=user
+            )
