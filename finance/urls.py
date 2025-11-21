6283ede925a0434832e1699e6bd2c8cf877929ad
Vadym <kirilkovadim193@gmail.com>
2025-11-21 13:47:45 +0200

    FEAT: Implement Authentication system(Login, Logout, Signup)

--- finance/urls.py
+++ finance/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from finance.views import AccountListView, TransactionCreateView
+from finance.views import AccountListView, SignUpView, TransactionCreateView
 
 urlpatterns = [
     path("", AccountListView.as_view(), name="account_list"),
     path("new/", TransactionCreateView.as_view(), name="transaction_create"),
+    path("signup/", SignUpView.as_view(), name="signup"),
 ]
--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
@@ -38,3 +39,9 @@ class TransactionCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
