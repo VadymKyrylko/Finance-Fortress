06532b58a4944c958a4207e4cca3f9cdf3eeda6c
Vadym <kirilkovadim193@gmail.com>
2025-11-25 17:18:40 +0200

    FEAT: Refactor signals(helper funcs), implement pre-save signal

--- finance/signals.py
+++ finance/signals.py
@@ -1,46 +1,66 @@
-from django.db.models.signals import post_delete, post_save
+from django.db.models.signals import post_delete, post_save, pre_save
 from django.dispatch import receiver
 
 from finance.models import Transaction
 
 
+def apply_transaction_effect(transaction):
+    """Applies the transaction's impact to balances (as when created)."""
+    if transaction.type == "INCOME":
+        transaction.account.balance += transaction.amount
+    elif transaction.type == "EXPENSE":
+        transaction.account.balance -= transaction.amount
+    elif transaction.type == "TRANSFER":
+        transaction.account.balance -= transaction.amount
+        if transaction.target_account:
+            transaction.target_account.balance += transaction.amount
+            transaction.target_account.save()
+    transaction.account.save()
+
+
+def revert_transaction_effect(transaction):
+    """Undoes the transaction's effect on balances (as in deletion)."""
+    if transaction.type == "INCOME":
+        transaction.account.balance -= transaction.amount
+    elif transaction.type == "EXPENSE":
+        transaction.account.balance += transaction.amount
+    elif transaction.type == "TRANSFER":
+        transaction.account.balance += transaction.amount
+        if transaction.target_account:
+            transaction.target_account.balance -= transaction.amount
+            transaction.target_account.save()
+
+    transaction.account.save()
+
+
+@receiver(pre_save, sender=Transaction)
+def handle_transaction_update(sender, instance, **kwargs):
+    """
+    Called BEFORE saving.
+    If the transaction already exists (this is an edit), we need to
+    find the old version of it in the database and undo its effects.
+    """
+    if instance.pk:
+        try:
+            old_instance = sender.objects.get(pk=instance.pk)
+            revert_transaction_effect(old_instance)
+        except sender.DoesNotExist:
+            pass
+
+
 @receiver(post_save, sender=Transaction)
 def update_balance_on_save(sender, instance, created, **kwargs):
     """
-    Called automatically AFTER saving the transaction.
-    Works only for NEW transactions (created=True).
+    Called AFTER saving.
+    Apply new data to balance.
     """
-    if created:
-        account = instance.account
-        amount = instance.amount
-
-        if instance.type == "INCOME":
-            account.balance += amount
-        elif instance.type == "EXPENSE":
-            account.balance -= amount
-        elif instance.type == "TRANSFER":
-            account.balance -= amount
-            if instance.target_account:
-                instance.target_account.balance += amount
-                instance.target_account.save()
-        account.save()
+    apply_transaction_effect(instance)
 
 
 @receiver(post_delete, sender=Transaction)
 def update_balance_on_delete(sender, instance, **kwargs):
     """
-    Called automatically AFTER deleting a transaction.
-    We need to 'roll back' the balance changes.
+    Called AFTER deletion.
+    Undo the effect of the transaction.
     """
-    account = instance.account
-    amount = instance.amount
-    if instance.type == "INCOME":
-        account.balance -= amount
-    elif instance.type == "EXPENSE":
-        account.balance += amount
-    elif instance.type == "TRANSFER":
-        account.balance += amount
-        if instance.target_account:
-            instance.target_account.balance -= amount
-            instance.target_account.save()
-    account.save()
+    revert_transaction_effect(instance)
